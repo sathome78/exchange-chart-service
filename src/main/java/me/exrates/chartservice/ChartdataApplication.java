518c34337a73b93a9dcb9a420d97bfb730f05fad
Maxim Peresadko <maxim.peresadko@upholding.biz>
2019-07-02 16:15:14 +0300

    init

---
+++ src/main/java/me/exrates/chartservice/ChartdataApplication.java
@@ -0,0 +1,15 @@
+package me.exrates.chartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ChartdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChartdataApplication.class, args);
+    }
+
+}
---
+++ src/main/java/me/exrates/chartservice/controller/ChartDataController.java
@@ -0,0 +1,19 @@
+package me.exrates.chartservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChartDataController {
+
+    @Autowired
+    private Environment env;
+
+    @GetMapping("/status/check")
+    public String status()
+    {
+        return "Working on port " + env.getProperty("local.server.port");
+    }
+}
