0a075ef187949f53cee25cebba361bd72a97e828
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2019-07-03 17:06:37 +0300

    DEVEX-2534 (rewrite elasticsearch business logic / add end to end test/ fix dependencies)

--- src/main/java/me/exrates/chartservice/services/impl/ElasticsearchProcessingServiceImpl.java
+++ src/main/java/me/exrates/chartservice/services/impl/ElasticsearchProcessingServiceImpl.java
@@ -8,6 +8,7 @@ import me.exrates.chartservice.model.CandleModel;
 import me.exrates.chartservice.services.ElasticsearchProcessingService;
 import org.elasticsearch.action.DocWriteResponse;
 import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
@@ -18,6 +19,8 @@ import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.reindex.BulkByScrollResponse;
+import org.elasticsearch.index.reindex.DeleteByQueryRequest;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,13 +35,15 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static java.util.Objects.isNull;
+
 @Log4j2
 @Service
 public class ElasticsearchProcessingServiceImpl implements ElasticsearchProcessingService {
 
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd_MM_yyyy_HH_mm");
 
-    private static final String INDEX = "_chartData";
+    private static final String INDEX = "chart";
 
     private final RestHighLevelClient client;
     private final ObjectMapper mapper;
@@ -54,59 +59,114 @@ public class ElasticsearchProcessingServiceImpl implements ElasticsearchProcessi
     }
 
     @Override
-    public void send(CandleModel model, String pairName) {
+    public boolean exist(String pairName, LocalDateTime dateTime) {
+        try {
+            GetRequest request = new GetRequest(INDEX, pairName, dateTime.format(FORMATTER));
+
+            return client.existsSource(request, RequestOptions.DEFAULT);
+        } catch (IOException ex) {
+            log.error("Problem with getting response from elasticsearch cluster", ex);
+            return false;
+        }
+    }
+
+    @Override
+    public CandleModel get(String pairName, LocalDateTime dateTime) {
+        try {
+            GetRequest request = new GetRequest(INDEX, pairName, dateTime.format(FORMATTER));
+
+            GetResponse response = client.get(request, RequestOptions.DEFAULT);
+
+            return mapper.readValue(response.getSourceAsString(), CandleModel.class);
+        } catch (IOException ex) {
+            log.error("Problem with getting response from elasticsearch cluster", ex);
+            return null;
+        }
+    }
+
+    @Override
+    public void insert(CandleModel model, String pairName) {
         xSync.execute(pairName, () -> {
-            String sourceString;
+            String sourceString = getSourceString(model);
+            if (isNull(sourceString)) {
+                return;
+            }
+            IndexRequest request = new IndexRequest(INDEX, pairName, model.getCandleOpenTime().format(FORMATTER))
+                    .source(sourceString, XContentType.JSON);
+
+            IndexResponse response;
             try {
-                sourceString = mapper.writeValueAsString(model);
-            } catch (JsonProcessingException ex) {
-                log.warn("Problem with write model object to string", ex);
+                response = client.index(request, RequestOptions.DEFAULT);
+            } catch (IOException ex) {
+                log.error("Problem with getting response from elasticsearch cluster", ex);
                 return;
             }
 
-            final String id = model.getCandleOpenTime().format(FORMATTER);
-
-            try {
-                GetRequest getRequest = new GetRequest(INDEX, pairName, id);
-
-                boolean exist = client.existsSource(getRequest, RequestOptions.DEFAULT);
-
-                if (exist) {
-                    UpdateRequest updateRequest = new UpdateRequest(INDEX, pairName, id)
-                            .doc(sourceString, XContentType.JSON);
-
-                    UpdateResponse updateResponse = client.update(updateRequest, RequestOptions.DEFAULT);
-
-                    if (updateResponse.getResult() != DocWriteResponse.Result.UPDATED) {
-                        log.warn("Source have not updated in elasticsearch cluster");
-                    }
-                } else {
-                    IndexRequest indexRequest = new IndexRequest(INDEX, pairName, id)
-                            .source(sourceString, XContentType.JSON);
-
-                    IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-
-                    if (indexResponse.getResult() != DocWriteResponse.Result.CREATED) {
-                        log.warn("Source have not created in elasticsearch cluster");
-                    }
-                }
-            } catch (IOException ex) {
-                log.warn("Problem with getting response from elasticsearch cluster", ex);
+            if (response.getResult() != DocWriteResponse.Result.CREATED) {
+                log.warn("Source have not created in elasticsearch cluster");
             }
         });
     }
 
     @Override
-    public List<CandleModel> get(LocalDateTime fromDate, LocalDateTime toDate, String pairName) {
+    public void update(CandleModel model, String pairName) {
+        xSync.execute(pairName, () -> {
+            String sourceString = getSourceString(model);
+            if (isNull(sourceString)) {
+                return;
+            }
+            UpdateRequest request = new UpdateRequest(INDEX, pairName, model.getCandleOpenTime().format(FORMATTER))
+                    .doc(sourceString, XContentType.JSON);
+
+            UpdateResponse response;
+            try {
+                response = client.update(request, RequestOptions.DEFAULT);
+            } catch (IOException ex) {
+                log.error("Problem with getting response from elasticsearch cluster", ex);
+                return;
+            }
+
+            if (response.getResult() != DocWriteResponse.Result.UPDATED) {
+                log.warn("Source have not updated in elasticsearch cluster");
+            }
+        });
+    }
+
+    private String getSourceString(final CandleModel model) {
         try {
-            SearchRequest searchRequest = new SearchRequest()
-                    .indices(INDEX)
+            return mapper.writeValueAsString(model);
+        } catch (JsonProcessingException ex) {
+            log.error("Problem with writing model object to string", ex);
+            return null;
+        }
+    }
+
+    @Override
+    public long deleteAll() {
+        try {
+            DeleteByQueryRequest deleteByQueryRequest = new DeleteByQueryRequest(INDEX);
+
+            BulkByScrollResponse response = client.deleteByQuery(deleteByQueryRequest, RequestOptions.DEFAULT);
+
+            return response.getDeleted();
+        } catch (IOException ex) {
+            log.warn("Problem with getting response from elasticsearch cluster", ex);
+
+            return 0L;
+        }
+    }
+
+    @Override
+    public List<CandleModel> getByQuery(LocalDateTime fromDate, LocalDateTime toDate, String pairName) {
+        try {
+            SearchRequest request = new SearchRequest()
                     .source(new SearchSourceBuilder()
                             .query(QueryBuilders.typeQuery(pairName))
                             .query(QueryBuilders.rangeQuery("candleOpenTime").from(fromDate).to(toDate)));
 
-            SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
-            return getSearchResult(searchResponse);
+            SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+
+            return getSearchResult(response);
         } catch (IOException ex) {
             log.warn("Problem with getting response from elasticsearch cluster", ex);
 
@@ -115,7 +175,6 @@ public class ElasticsearchProcessingServiceImpl implements ElasticsearchProcessi
     }
 
     private List<CandleModel> getSearchResult(SearchResponse response) {
-
         SearchHit[] searchHit = response.getHits().getHits();
 
         List<CandleModel> chartData = new ArrayList<>();
