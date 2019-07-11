078a59d565d61d31503465aa993b2a4417e46585
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2019-07-11 11:16:41 +0300

    DEVEX-2537 (return back some code)

--- src/main/java/me/exrates/chartservice/services/TradeDataService.java
+++ src/main/java/me/exrates/chartservice/services/TradeDataService.java
@@ -3,5 +3,6 @@ package me.exrates.chartservice.services;
 import me.exrates.chartservice.model.TradeDataDto;
 
 public interface TradeDataService {
+
     void handleReceivedTrade(TradeDataDto message);
 }
--- src/main/java/me/exrates/chartservice/services/messaging/RabbitListeners.java
+++ src/main/java/me/exrates/chartservice/services/messaging/RabbitListeners.java
@@ -2,7 +2,9 @@ package me.exrates.chartservice.services.messaging;
 
 import lombok.extern.log4j.Log4j2;
 import me.exrates.chartservice.model.TradeDataDto;
+import me.exrates.chartservice.services.TradeDataService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -13,11 +15,19 @@ import static me.exrates.chartservice.configuration.CommonConfiguration.MODULE_M
 @Component
 public class RabbitListeners {
 
+    private final TradeDataService tradeDataService;
+
+    @Autowired
+    public RabbitListeners(TradeDataService tradeDataService) {
+        this.tradeDataService = tradeDataService;
+    }
+
 
     @RabbitListener(queues = "${spring.rabbitmq.tradestopic}")
     public void recieveTrade(TradeDataDto message) {
         System.out.println("income message !!!");
         System.out.println(message);
         /*handle messages here*/
+        tradeDataService.handleReceivedTrade(message);
     }
 }
