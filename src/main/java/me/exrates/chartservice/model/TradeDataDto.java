ff4ec382be5cb1fb79c7144ed735538fcdec887c
Maxim Peresadko <maxim.peresadko@upholding.biz>
2019-07-19 12:13:44 +0300

    tradeDataService tests

--- src/main/java/me/exrates/chartservice/model/TradeDataDto.java
+++ src/main/java/me/exrates/chartservice/model/TradeDataDto.java
@@ -3,15 +3,18 @@ package me.exrates.chartservice.model;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import me.exrates.chartservice.model.serializers.CurrencyPairDeserializer;
 import me.exrates.chartservice.model.serializers.LocalDateTimeDeserializer;
 import me.exrates.chartservice.model.serializers.LocalDateTimeSerializer;
+import me.exrates.chartservice.model.OrderDto;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Data
+@Getter@Setter
 @NoArgsConstructor
 public class TradeDataDto {
 
@@ -25,6 +28,7 @@ public class TradeDataDto {
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime tradeDate;
 
+
     public TradeDataDto(OrderDto order) {
         this.orderId = order.getId();
         this.pairName = order.getCurrencyPairName();
@@ -32,5 +36,12 @@ public class TradeDataDto {
         this.amountBase = order.getAmountBase();
         this.amountConvert = order.getAmountConvert();
         this.tradeDate = order.getDateAcception();
+    }
+
+    public TradeDataDto(String pairName, BigDecimal exrate, BigDecimal amountBase, LocalDateTime tradeDate) {
+        this.pairName = pairName;
+        this.exrate = exrate;
+        this.amountBase = amountBase;
+        this.tradeDate = tradeDate;
     }
 }
